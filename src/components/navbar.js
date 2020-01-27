dcd69ba5caa4d2ee417d7218a138ded3f08c339b
Alex Tilatti <alex.tilatti@gmail.com>
2020-01-26 21:39:00 -0500

    removed notes from navbar css

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -4,27 +4,25 @@ import "../styles/navbar.css";
 export default class Navbar extends React.Component {
   render() {
     return (
-      <div>
-        <nav className="navbar navbar-expand-lg navbar-dark text-white bg-primary">
-          <span className="navbar-brand"> Swapi Skywalker </span>
-          <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
-            <span className="navbar-toggler-icon"></span>
-          </button>
-          <div className="collapse navbar-collapse" id="navbarText">
-            <ul className="navbar-nav ml-auto">
-              <li className="nav-item">
-                <a className="nav-link" href="#">Home</a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="#">About</a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="#">Contact</a>
-              </li>
-            </ul>
-          </div>
-        </nav>
-      </div>
+      <nav className="navbar navbar-expand-lg navbar-dark text-white bg-primary">
+        <span className="navbar-brand"> Swapi Skywalker </span>
+        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarText">
+          <ul className="navbar-nav ml-auto">
+            <li className="nav-item">
+              <a className="nav-link" href="#">Home</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">About</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">Contact</a>
+            </li>
+          </ul>
+        </div>
+      </nav> 
     )
   }
 }
